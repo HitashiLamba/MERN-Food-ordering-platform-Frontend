516105329c0aa6cc2692e07754213317604f207f
Hitashi <hitashilamba2@gmail.com>
2024-07-13 07:56:00 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+
+import path from "path" //it tells about type of node
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
